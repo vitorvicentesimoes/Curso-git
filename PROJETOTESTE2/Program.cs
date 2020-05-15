5c0ad74bd3cb5d86d0f4f1a695a9a564fcd3bab5
Vitor Vicente <vitorjump@outlook.com>
2020-05-15 15:14:16 -0300

    Acrescentada a mensagem: desejanto um bom dia

--- PROJETOTESTE2/Program.cs
+++ PROJETOTESTE2/Program.cs
@@ -4,8 +4,8 @@ namespace PROJETOTESTE2 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia !");
             Console.WriteLine("Como voce esta ?");
+            Console.WriteLine("Tenha um bom dia ");
             Console.ReadLine();
            
         }
